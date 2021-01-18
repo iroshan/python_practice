f223fc4d2b8294d4355b4fd80ff6c3d6735f5222
Iroshan <iroshan464@gmail.com>
2021-01-18 11:07:10 +0530

    corrected the implementation. still have many indentation erros

--- bingo_cards.py
+++ bingo_cards.py
@@ -1,22 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random
 
 # create the card dictionary
-bingo_cards={}
-n=1
+
+bingo_cards = {}
+n = 1
 for c in 'BINGO':
-  bingo_cards[c] =[i for i in range(n,n+15)]
-  n +=15
- 
- 
-# function to create and print a random bingo card
-def print_card():
+    bingo_cards[c] = [i for i in range(n, n + 15)]
+    n += 15
+
+# function to createa random bingo card
+def create_card():
+	card = {k:(random.sample(bingo_cards[k],5)) for k in bingo_cards.keys()}
+	return card
+
+
+# print the card
+def print_card(card):
 	print(*bingo_cards.keys(),sep='\t')
-	for _ in range(5):
-		card = {k:(random.choice(bingo_cards[k])) for k in bingo_cards.keys()}
-		print(*card.values(),sep='\t')
+	transpose = zip(*card.values())
+	for row in transpose:
+		print(*row, sep ='\t')
+
+
 
 
 if __name__ == '__main__':
-	print_card()
+	print_card(create_card())
 
 
