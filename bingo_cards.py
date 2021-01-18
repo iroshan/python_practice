e028c7b84ee956b90472d8064f7b98df8e3ba1f8
iroshan <iroshan464@gmail.com>
2021-01-18 12:24:16 +0530

    Indentation messup

--- bingo_cards.py
+++ bingo_cards.py
@@ -7,23 +7,22 @@ import random
 bingo_cards = {}
 n = 1
 for c in 'BINGO':
-bingo_cards[c] = [i for i in range(n, n + 15)]
-n += 15
+    bingo_cards[c] = [i for i in range(n, n + 15)]
+    n += 15
 
 #function to createa random bingo card
 def create_card():
-card = {
-	k: (random.sample(bingo_cards[k], 5)) for k in bingo_cards.keys()}
-return card
+    card = {k: (random.sample(bingo_cards[k], 5)) for k in bingo_cards.keys()}
+    return card
 
 
 # print the card
 def print_card(card):
-print(* bingo_cards.keys(), sep = '\t')
-transpose = zip(* card.values())
-for row in transpose:
-print(* row, sep = '\t')
+    print(* bingo_cards.keys(), sep = '\t')
+    transpose = zip(* card.values())
+    for row in transpose:
+        print(* row, sep = '\t')
 
 
 if __name__ == '__main__':
-print_card(create_card())
+    print_card(create_card())
