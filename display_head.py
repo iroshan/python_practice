d03b3ef2231a5fb49d33db39235b6412ed651ce1
iroshan <iroshan464@gmail.com>
2021-01-19 13:44:43 +0530

    Edited comments

--- display_head.py
+++ display_head.py
@@ -1,5 +1,13 @@
 """
-Unix-based operating systems usually include a tool named head. It displays the first 10 lines of a file whose name is provided as a command line parameter. Write a Python program that provides the same behavior. Display an appropriate error message if the file requested by the user does not exist or if the command line parameter is omitted.
+Unix-based operating systems usually
+include a tool named head. 
+It displays the first 10 lines of a file 
+whose name is provided as a command line parameter. 
+Write a Python program that provides 
+the same behavior. 
+Display an appropriate error message 
+if the file requested by the user does not exist 
+or if the command line parameter is omitted.
 """
 import os
 
