1b61a95c437818b085d32f6f58c6c846bfe236b4
Iroshan <iroshan464@gmail.com>
2021-01-20 06:34:06 +0530

    Coding Challenge: Day 35: Display the Tail of a File

--- tail.py
+++ tail.py
@@ -1,8 +1,9 @@
 '''
-Unix-based operating systems usually include a tool named head. 
-It displays the first 10 lines of a file whose name is provided as a command line parameter. 
+Unix-based operating systems also typically include a tool named tail. 
+It displays the last 10 lines of a file whose name is provided as a command line parameter. 
 Write a Python program that provides the same behavior. 
-Display an appropriate error message if the file requested by the user does not exist or if the command line parameter is omitted.
+Display an appropriate error message if the file requested by the user does not exist or if the command line parameter is omitted...
+
 '''
 
 import sys
