14af43ff94347e651b5e1322bc5977d18575c794
Iroshan <iroshan464@gmail.com>
2021-01-24 07:03:45 +0530

    dealing with # in the middle of line

--- remove_comments.py
+++ remove_comments.py
@@ -7,7 +7,7 @@ Check each line in the file to determine if a # character is present.
 If it is then your program should remove all of the characters from the # character to the end of the line
 (we’ll ignore the situation where the comment character occurs inside of a string). Save the modified file using a new name that will be entered by the user.
 The user will also enter the name of the input file.
-Ensure that an appropriate error message is displayed if a problem is encountered while accessing the files. 
+Ensure that an appropriate error message is displayed if a problem is encountered while accessing the files.
 '''
 
 import os
