09eb397e5528560f27e2a169be0d62b2270fa21a
iroshan <iroshan464@gmail.com>
2021-01-27 08:43:28 +0530

    update redact.py

--- redact.py
+++ redact.py
@@ -1,3 +1,7 @@
+import re
+
+# This is my initial function.
+# not case sensitive
 def redact(write_file, word_file):
     # open the redact words list file and make a set of words
     with open(word_file,'r') as f:
@@ -8,5 +12,34 @@ def redact(write_file, word_file):
         file_data = f.read()
         # replace each word in file if the word is in word_list
         for word in word_list:
-            file_data = file_date.replace(word, '*'*len(word))
-        f.truncate()
+            file_data = file_data.replace(word.lower(), '*'*len(word))
+        f.truncate(0)
+        f.write(file_data)
+
+
+# This uses regex and vase insensitive.
+# issues:
+# loads the file in to memory. read the entire file for each word.
+# is there a better way?
+def redact_ignore_case(write_file, word_file):
+    with open(word_file) as f:
+        word_list = set(f.read().lower().split())
+    with open(write_file,'r+') as f:
+        file_data = f.read()
+        for word in word_list:
+            file_data = re.sub(word,'*'*len(word), file_data, flags= re.IGNORECASE)
+            f.truncate(0)
+            f.write(file_data)
+    
+
+def main():
+    try:
+        to_redact = input("enter file name to redact: ")
+        words = input("enter the file name with the words to redact: ")
+        redact_ignore_case(to_redact, words)
+        print("redaction complete...")
+    except Exception as e:
+        print(f"error occured. \n{e}")
+
+if __name__ == "__main__":
+    main()
