1a17c15e36acdaa45cab82bd9ecfa76e8706d04d
Iroshan <iroshan464@gmail.com>
2021-02-08 06:03:10 +0530

    docs

--- pig_latin.py
+++ pig_latin.py
@@ -1,11 +1,12 @@
 def pig_latin(word):
+  """translate the word to pig latin"""
   if word[0] in 'aeiou':
     return word+"way"
   else:
     return word[1:]+word[0]+'ay'
 
 while True:
-  word = input('Enter the word to ranslate to pig latin: ').strip().lower()
+  word = input('Enter the word to ranslate to pig latin: (blank to exit) ').strip().lower()
   if word:
     print(pig_latin(word))
   else:
