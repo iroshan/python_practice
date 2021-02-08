f9de7bfd48755dfab75d6168e517f94fca2be94b
iroshan <iroshan464@gmail.com>
2021-02-08 21:35:49 +0530

    Day 55: Pig Latin Sentence

---
+++ pl_sentence.py
@@ -0,0 +1,20 @@
+def pl_sentence(word):
+  """ returns a sentence converted to pig latin.
+  capital letters, punctuations are not supported"""
+  if word[0] in 'aeiou':
+    return word+"way"
+  else:
+    return word[1:]+word[0]+'ay'
+
+def main():
+    """main program"""
+    while True:
+      sentence = input('Enter the sentence to translate to pig latin (blank to quit: ').strip()
+      if not sentence:
+        print("not a sentence")
+        break
+      words = [pl_sentence(word) for word in sentence.lower().split()]
+      print(" ".join(words))
+      
+if __name__ == "__main__" :
+    main()
