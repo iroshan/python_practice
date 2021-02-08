bfe4360c2056d5e2b73db456d8b55cdedb65b28f
Iroshan <iroshan464@gmail.com>
2021-02-08 06:05:28 +0530

    indentation

--- pig_latin.py
+++ pig_latin.py
@@ -1,14 +1,16 @@
 def pig_latin(word):
-  """translate the word to pig latin"""
-  if word[0] in 'aeiou':
-    return word+"way"
-  else:
-    return word[1:]+word[0]+'ay'
+    """translate the word to pig latin"""
+    if word[0] in 'aeiou':
+        return word+"way"
+    else:
+        return word[1:]+word[0]+'ay'
+
 
 while True:
-  word = input('Enter the word to ranslate to pig latin: (blank to exit) ').strip().lower()
-  if word:
-    print(pig_latin(word))
-  else:
-    print('exiting...')
-    break
+    word = input(
+        'Enter the word to ranslate to pig latin: (blank to exit) ').strip().lower()
+    if word:
+        print(pig_latin(word))
+    else:
+        print('exiting...')
+        break
