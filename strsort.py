9f55a9f416a7bee1b055e36ce70cedb26bd747fe
iroshan <iroshan464@gmail.com>
2021-02-11 04:49:11 +0530

    Create strsort.py

---
+++ strsort.py
@@ -0,0 +1,20 @@
+def strsort_ignorecase(word):
+  """returns characters of the word sorted, ignoring case"""
+  return ''.join(sorted(word, key=lambda s: ord(s.lower())))
+  
+def strsort(word):
+  """returns sorted word"""
+  return "".join(sorted(word))
+  
+  
+def main():
+  while True:
+    word = input('enter word to sort (blank to exit): ').strip()
+    if not word:
+      print('no word. exiting...')
+      break
+    print(strsort(word))
+    
+    
+if __name__ == '__main__':
+  main()
