2e05f0441f2b4c1022f39415d9d14c2c7610a0ac
Iroshan <iroshan464@gmail.com>
2021-02-11 04:56:46 +0530

    indentations

--- strsort.py
+++ strsort.py
@@ -1,20 +1,21 @@
 def strsort_ignorecase(word):
-  """returns characters of the word sorted, ignoring case"""
-  return ''.join(sorted(word, key=lambda s: ord(s.lower())))
-  
+    """returns characters of the word sorted, ignoring case"""
+    return ''.join(sorted(word, key=lambda s: ord(s.lower())))
+
+
 def strsort(word):
-  """returns sorted word"""
-  return "".join(sorted(word))
-  
-  
+    """returns sorted word"""
+    return "".join(sorted(word))
+
+
 def main():
-  while True:
-    word = input('enter word to sort (blank to exit): ').strip()
-    if not word:
-      print('no word. exiting...')
-      break
-    print(strsort(word))
-    
-    
+    while True:
+        word = input('enter word to sort (blank to exit): ').strip()
+        if not word:
+            print('no word. exiting...')
+            break
+        print(strsort(word))
+
+
 if __name__ == '__main__':
-  main()
+    main()
