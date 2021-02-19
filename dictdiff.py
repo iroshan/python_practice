e5c348d09500002e28a512025fd4528dcad74f55
Iroshan <iroshan464@gmail.com>
2021-02-19 17:28:46 +0530

    minor edits

--- dictdiff.py
+++ dictdiff.py
@@ -2,19 +2,19 @@
 def dictdiff(a, b):
     """returns the difference between a and b dictionaries """
     # new dict to add data
-    c ={}
+    c = {}
     # make a set with unique keys
-    for k in set([*a.keys(),*b.keys()]):
+    for k in set([*a.keys(), *b.keys()]):
         # if both items are same, pass
-        if a.get(k,None) == b.get(k,None):
+        if a.get(k, None) == b.get(k, None):
             continue
         # else add to new dictionary
         else:
-            c[k] = [a.get(k,None), b.get(k,None)]
+            c[k] = [a.get(k, None), b.get(k, None)]
     return c
-            
-    
-if __name__ =="__main__":
-    a = {"a":1, "b":2, "c":3}
-    b = {"a":1, "b":3, "c":5, "d":4}
-    print(dictdiff(a,b))
+
+
+if __name__ == "__main__":
+    a = {"a": 1, "b": 2, "c": 3}
+    b = {"a": 1, "b": 3, "c": 5, "d": 4}
+    print(dictdiff(a, b))
