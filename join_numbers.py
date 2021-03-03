444a2f1998917bc67735cfee07cabc6a70621a12
iroshan <iroshan464@gmail.com>
2021-03-03 08:55:07 +0530

    Update join_numbers.py

--- join_numbers.py
+++ join_numbers.py
@@ -1,6 +1,6 @@
 def join_numbers(n: int) -> str:
     """returns the numbers from 0 to n as a string"""
-    return ",".join([str(i) for i in range(n+1)])
+    return ",".join(str(i) for i in range(n+1))
 
 
 if __name__ == "__main__":
